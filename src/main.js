76c9e8bcf8bdacbf7195c93dcc704a6b36f4abc5
Adilet312 <adiletm@hotmail.com>
2021-02-09 23:50:48 -0800

    Add some problems.

--- src/main.js
+++ src/main.js
@@ -1,65 +1,26 @@
 import './styles.css';
+
 /*
-VinSolutions Coding Assessment
-In the programming language of your choice, write a program that parses a sentence and replaces each word with the following: first letter, number of distinct characters between first and last character, and last letter.  For example, Smooth would become S3h.  Words are separated by spaces or non-alphabetic characters and these separators should be maintained in their original form and location in the answer.
+fetch data and do debounce
+to do list
+clock project
 */
-let sentenceInput = document.querySelector('.sentence');
-const form =  document.querySelector('#formId');
-let output =  document.querySelector('.output');
-
-form.addEventListener('submit',submitForm);
- /*Read */
-function submitForm(e){
-  e.preventDefault();
-  let sentence =  sentenceInput.value.trim();
-  if(/[A-Za-z]/.test(sentence )){
-    setSuccess(sentenceInput, 'Success value');
-    output.innerHTML = parsingFunction(sentence);
-  }else{
-    setError(sentenceInput, 'Please enter proper value')
+/*
+------Find missing number
+------Find duplicate number
+------Remove similar words
+------Find longest palindrome
+------Count palindromic substrings
+------Find longest substring
+------Flattern nested array
+------LongestWord
+------Union , Intersection, Differences, and Simmetric Differences
+*/
+const createArray = (elementArray, indexArray) => {
+  const target = []
+  for( let idx = 0; idx < elementArray.length; idx++){
+    target.splice(indexArray[idx], 0 , elementArray[idx])
   }
+  return target;
 }
-
-/*Set error message on the browser by giving class name that class will change color in CSS*/
-function setError(input, errorMessage){
-  let message = input.nextElementSibling;
-  message.classList.add('error');
-  message.innerHTML = errorMessage;
-}
-/*Set success message on the browser by giving class name that class will change color in CSS*/
-function setSuccess(input, successMessage){
-  let message = input.nextElementSibling;
-  message.classList.add('success');
-  message.innerHTML = successMessage;
-}
-
-/*Parse sentence into special pattern*/
-function parsingFunction(input) {
-    /*Remove all symbols or leave only letters from a-z or A-Z*/
-    let stringWithoutSymbols = input.replace(/[^a-zA-Z ]/g, "");
-    /*Split each word of sentence as a element of array*/
-    let arr = stringWithoutSymbols.split(" ");
-
-    let newStr =   arr.reduce( (acc, word) => {
-        let sizeWord = word.length - 1;
-        let newStr = word.substring(0,1)+ `${countUniqueLetters(word.substring(1,sizeWord))}` + word.substring(sizeWord)
-        acc.push(newStr)
-        return acc;
-      },[])
-      return newStr.join(' ');
-    }
-
-  /*Count non-consecitve letters. For example: Smooth => Smoth or Smoooth => Smoth */
- function countUniqueLetters(str){
-   let arr = str.split('');
-   //Smoooth
-   let index = 0;
-   while(index < arr.length){
-     if(arr[index] === arr[index + 1]){
-       arr.splice(index, 1)
-     }else{
-       index++;
-     }
-   }
-   return arr.length;
- }
+console.log(createArray([0,1,2,3,4],[0,1,2,2,1]));
---
+++ src/vimSolution.js
@@ -0,0 +1,64 @@
+/*
+VinSolutions Coding Assessment
+In the programming language of your choice, write a program that parses a sentence and replaces each word with the following: first letter, number of distinct characters between first and last character, and last letter.  For example, Smooth would become S3h.  Words are separated by spaces or non-alphabetic characters and these separators should be maintained in their original form and location in the answer.
+*/
+let sentenceInput = document.querySelector('.sentence');
+const form =  document.querySelector('#formId');
+let output =  document.querySelector('.output');
+
+form.addEventListener('submit',submitForm);
+ /*Read */
+function submitForm(e){
+  e.preventDefault();
+  let sentence =  sentenceInput.value.trim();
+  if(/[A-Za-z]/.test(sentence )){
+    setSuccess(sentenceInput, 'Success value');
+    output.innerHTML = parsingFunction(sentence);
+  }else{
+    setError(sentenceInput, 'Please enter proper value')
+  }
+}
+
+/*Set error message on the browser by giving class name that class will change color in CSS*/
+function setError(input, errorMessage){
+  let message = input.nextElementSibling;
+  message.classList.add('error');
+  message.innerHTML = errorMessage;
+}
+/*Set success message on the browser by giving class name that class will change color in CSS*/
+function setSuccess(input, successMessage){
+  let message = input.nextElementSibling;
+  message.classList.add('success');
+  message.innerHTML = successMessage;
+}
+
+/*Parse sentence into special pattern*/
+function parsingFunction(input) {
+    /*Remove all symbols or leave only letters from a-z or A-Z*/
+    let stringWithoutSymbols = input.replace(/[^a-zA-Z ]/g, "");
+    /*Split each word of sentence as a element of array*/
+    let arr = stringWithoutSymbols.split(" ");
+
+    let newStr =   arr.reduce( (acc, word) => {
+        let sizeWord = word.length - 1;
+        let newStr = word.substring(0,1)+ `${countUniqueLetters(word.substring(1,sizeWord))}` + word.substring(sizeWord)
+        acc.push(newStr)
+        return acc;
+      },[])
+      return newStr.join(' ');
+    }
+
+  /*Count non-consecitve letters. For example: Smooth => Smoth or Smoooth => Smoth */
+ function countUniqueLetters(str){
+   let arr = str.split('');
+   //Smoooth
+   let index = 0;
+   while(index < arr.length){
+     if(arr[index] === arr[index + 1]){
+       arr.splice(index, 1)
+     }else{
+       index++;
+     }
+   }
+   return arr.length;
+ }
